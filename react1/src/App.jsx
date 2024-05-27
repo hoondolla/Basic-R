bb9872052db500e950728242b71175125bbad882
hoondolla <gnswo9187@daum.net>
2024-05-27 20:17:47 +0900

    리액트 1강 과제

--- react1/src/App.jsx
+++ react1/src/App.jsx
@@ -1,9 +1,33 @@
 import './App.css'
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [count, setCount] = useState("0");
+
+  const addCount = () => {
+    setCount(count + 1);
+  }
+
+  const disCount = () => {
+    setCount(count - 1);
+  }
+
+  const reMove = () => {
+    setCount(0)
+  }
+
+
   return (
-    <div>App</div>
+    <>
+    <div>
+    <button onClick={addCount}>증가</button>
+    Count : {count}
+    <button onClick={disCount}>감소</button>
+    </div>
+    <div>
+      <button onClick={reMove}>리셋</button>
+    </div>
+    </>
   )
 }
 
